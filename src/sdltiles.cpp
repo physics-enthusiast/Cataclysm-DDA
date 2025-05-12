23dbeed6de3b858ebe1ea737024dc0c8db30dc25
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2025-05-12 18:15:40 +0800

    Update sdltiles.cpp

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -2695,7 +2695,7 @@ static void CheckMessages()
                 JNIEnv *env = ( JNIEnv * )SDL_AndroidGetJNIEnv();
                 jobject activity = ( jobject )SDL_AndroidGetActivity();
                 jclass clazz( env->GetObjectClass( activity ) );
-                jstring toast_message = env->NewStringUTF( touch_input_context.get_category().c_str() );
+                jstring toast_message = env->NewStringUTF( touch_input_context.constructor_callsite.file_name() );
                 jmethodID method_id = env->GetMethodID( clazz, "toast", "(Ljava/lang/String;)V" );
                 env->CallVoidMethod( activity, method_id, toast_message );
                 env->DeleteLocalRef( activity );
